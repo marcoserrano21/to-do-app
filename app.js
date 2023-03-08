3136561bd31881a64b0f7480afa58f3fce2e7f55
Marco <103689125+marcoserrano21@users.noreply.github.com>
2023-03-08 17:15:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,6 @@
 // };
 
 
-// Initialize a variable to keep track of the number of tasks added
 var taskCount = 0;
 
 // Add a keydown event listener to the input field
@@ -59,39 +58,51 @@ document.querySelector('#add').onclick = function () {
         var containerOffset = -10 * taskCount;
 
         // Add the new task to the task list
-        document.querySelector('#tasks').innerHTML += `
+        var taskHTML = `
             <div class="task">
-                <label class="taskCheck">
+                <div class="taskCheck">
                     <input type="checkbox" class="taskCheckbox">
-                    <span id="taskname">
+                    <span class="taskname">
                         ${taskInput.value}
                     </span>
-                </label>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
+                </div>
+                <div class="taskButtons">
+                    <button class="editButton">Edit</button>
+                    <button class="deleteButton">Delete</button>
+                </div>
             </div>
         `;
+        document.querySelector('#tasks').insertAdjacentHTML('beforeend', taskHTML);
         taskInput.value = ""; // clear the input field
 
         // Move the container up by adjusting its margin
         document.querySelector('.container').style.marginTop = containerOffset + 'px';
 
-        // Add click event listeners to the delete buttons of all tasks
-        var current_tasks = document.querySelectorAll(".delete");
+        // Add click event listeners to the delete buttons and edit buttons of all tasks
+        var current_tasks = document.querySelectorAll(".task");
         for (var i = 0; i < current_tasks.length; i++) {
-            current_tasks[i].onclick = function () {
+            current_tasks[i].querySelector(".deleteButton").onclick = function () {
                 // Decrement the task count and recalculate the container offset
                 taskCount--;
                 containerOffset = -10 * taskCount;
 
                 // Remove the task from the task list
-                this.parentNode.remove();
+                this.parentNode.parentNode.remove();
 
                 // Move the container back down by adjusting its margin
                 document.querySelector('.container').style.marginTop = containerOffset + 'px';
             }
+
+            current_tasks[i].querySelector(".editButton").onclick = function () {
+                // Get the task name and prompt the user to enter a new task name
+                var taskName = this.parentNode.parentNode.querySelector(".taskname");
+                var newTaskName = prompt("Enter a new task name:", taskName.textContent);
+
+                // Update the task name if the user entered a new name
+                if (newTaskName !== null && newTaskName.trim() !== "") {
+                    taskName.textContent = newTaskName.trim();
+                }
+            }
         }
     }
 };
-
